6ded7a18376e8697abba8630448f4650ce14a2db
Swapnil Udar <swapnil.udar@tomtom.com>
2025-04-14 20:16:24 +0530

    setup conn with firestore and fetched data from it

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ehop_app/model/benefit.dart';
 import 'package:ehop_app/widget/benefit_card.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(home: const BenefitsPage()));
 }
 
 class BenefitsPage extends StatelessWidget {
-  static List<Benefit> benefits = [
-    Benefit("benefit 1", "description 1"),
-    Benefit("benefit 2", "description 2"),
-    Benefit("benefit 3", "description 3"),
-  ];
+  Future<List<Benefit>> getBenefits() async {
+    try {
+      QuerySnapshot<Map<String, dynamic>> querySnapshot =
+          await FirebaseFirestore.instance.collection('benefits_trial').get();
+
+      List<Benefit> benefits =
+          querySnapshot.docs.map((doc) {
+            return Benefit.fromFirestore(
+              doc,
+              null,
+            ); // Assuming your factory method can handle null options
+          }).toList();
+
+      return benefits;
+    } catch (e) {
+      print("Error getting benefits: $e");
+      return []; // Return an empty list in case of error
+    }
+  }
 
   const BenefitsPage({super.key});
 
@@ -23,16 +44,30 @@ class BenefitsPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: benefits.length,
-          itemBuilder: (BuildContext context, int index) {
-            return BenefitCard(
-              benefit: benefits[index],
-              onTap: () => {print("Tapped on ${benefits[index].name}")},
+      body: FutureBuilder<List<Benefit>>(
+        future: getBenefits(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final benefits = snapshot.data!;
+            return Center(
+              child: ListView.builder(
+                itemCount: benefits.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return BenefitCard(
+                    benefit: benefits[index],
+                    onTap: () => {print("Tapped on ${benefits[index].name}")},
+                  );
+                },
+              ),
             );
-          },
-        ),
+          } else {
+            return const Center(child: Text('No benefits found'));
+          }
+        },
       ),
     );
   }
--- lib/model/benefit.dart
+++ lib/model/benefit.dart
@@ -1,6 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Benefit {
   String name;
   String description;
 
   Benefit(this.name, this.description);
+
+  factory Benefit.fromFirestore(
+    QueryDocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    return Benefit(
+      data['name'] ?? '', // Provide default values if fields are missing
+      data['description'] ?? '',
+    );
+  }
 }

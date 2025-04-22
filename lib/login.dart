e1977878267b97b358674aabe28b38bfefa56d60
Swapnil Udar <swapnil.udar@tomtom.com>
2025-04-22 18:04:38 +0530

    introducing login page

---
+++ lib/login.dart
@@ -0,0 +1,85 @@
+import 'package:ehop_app/firebase_options.dart';
+import 'package:ehop_app/main.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MaterialApp(home: const EhopLoginPage()));
+}
+
+class EhopLoginPage extends StatelessWidget {
+  const EhopLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _otpController = TextEditingController();
+  bool _showOTPField = false;
+
+  void _getOTP() {
+    // Simulate OTP retrieval
+    setState(() {
+      _showOTPField = true;
+    });
+  }
+
+  void _login() {
+    // Simulate login process
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const BenefitsPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            if (_showOTPField)
+              TextField(
+                controller: _otpController,
+                decoration: const InputDecoration(labelText: 'OTP'),
+                keyboardType: TextInputType.number,
+              ),
+            const SizedBox(height: 20),
+            if (!_showOTPField)
+              ElevatedButton(onPressed: _getOTP, child: const Text('Get OTP')),
+            if (_showOTPField)
+              ElevatedButton(onPressed: _login, child: const Text('Login')),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ehop_app/model/benefit.dart';
 import 'package:ehop_app/widget/benefit_card.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MaterialApp(home: const BenefitsPage()));
-}
 
 class BenefitsPage extends StatelessWidget {
   Future<List<Benefit>> getBenefits() async {

19ce5cd71dcdf4dfa69034677a8a8f2e289ca6bc
Swapnil Udar <swapnil.udar@tomtom.com>
2025-04-22 23:52:58 +0530

    established communication between ehop-app and cloud function for OTP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,23 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
-export const requestOTP = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+interface RequestData {
+  emailAddress: string;
+}
+
+interface ResponseData {
+  status: string;
+  otp: number;
+}
+
+export const requestOTP = functions.https.onCall<RequestData, ResponseData>(
+  (request) => {
+    const email = request.data.emailAddress;
+    const generatedOtp = email.length;
+    return {status: "success", otp: generatedOtp};
+  },
+);
--- lib/login.dart
+++ lib/login.dart
@@ -2,6 +2,7 @@ import 'package:ehop_app/firebase_options.dart';
 import 'package:ehop_app/main.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +16,7 @@ class EhopLoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ehop login page',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -37,8 +38,13 @@ class LoginPageState extends State<LoginPage> {
   final TextEditingController _otpController = TextEditingController();
   bool _showOTPField = false;
 
-  void _getOTP() {
+  Future<void> _getOTP() async {
     // Simulate OTP retrieval
+    final functions = FirebaseFunctions.instanceFor(region: 'us-central1');
+    final result = await functions.httpsCallable('requestOTP').call(
+      <String, String>{'emailAddress': _emailController.text},
+    );
+    print('Cloud Function result: ${result.data}');
     setState(() {
       _showOTPField = true;
     });

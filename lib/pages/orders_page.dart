19cf9f57d541166b1a1333160917b4536712cb43
swapniludar <swapniludar@users.noreply.github.com>
2025-04-27 18:10:29 +0530

    Ehop p1 development (#1)
    * Master page and icons changes
    
    * Added healthcheck page
    
    ---------
    
    Co-authored-by: mamata2p <patil.mamata.p@gmail.com>

---
+++ lib/pages/orders_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OrdersPage extends StatelessWidget {
+  const OrdersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Orders Page", style: TextStyle(fontSize: 24)));
+  }
+}
